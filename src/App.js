b37320264cf3731f2de4096b7e60990f143a59e3
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-09-24 11:46:07 +0530

    Added styling to the editor section

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
 
   return (
     <div className="App">
-      <div className= "pannel top" >
+      <div className= "panel top" >
         <Editor lang="xml" title="HTML" value={html} onChange={setHtml}></Editor>
         <Editor lang="css" title="CSS" value={css} onChange={setCss}></Editor>
         <Editor lang="javascript" title="JS" value={js} onChange={setJs}></Editor>
       </div>
 
-      <div className= "pannel bottom" >
+      <div className= "panel bottom" >
         <iframe 
           title="output"
           sandbox="allow-scripts"
